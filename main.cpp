b588816290cfe7c231cb67af298125ee68b8d13c
414645 <414645@bsd48.org>
2023-01-11 13:08:30 -0800

    second commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//By Ryan Veith
+//
+//This is a test class for the linked lists project
+
+//void addStudent
+//void printStudent
+//void deleteStudent
+
+int main() {
+  cout << "hello world" << endl;
+  
+}
